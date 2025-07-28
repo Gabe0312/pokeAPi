c3c76728aefa8e856dbad2bda8a1dc38b8395af2
Gabe0312 <noslen120860@gmail.com>
2025-07-28 10:17:54 -0400

    added styling details

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.codepath.libraries:asynchttpclient:2.2.0")
+
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
